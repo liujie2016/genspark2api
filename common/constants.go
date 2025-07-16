58be4eef7a2003dec64e8d65b72559192b611a71
dean <1006393151@qq.com>
2025-07-16 20:24:24 +0800

    fix: v1.12.6

--- common/constants.go
+++ common/constants.go
@@ -3,22 +3,18 @@ package common
 import "time"
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v1.12.5"           // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v1.12.6"           // this hard coding will be replaced automatically when building, no need to manually change
 
 var DefaultOpenaiModelList = []string{
 	"gpt-4.1",
-	"o1",
-	"o3",
 	"o3-pro",
 	"o4-mini-high",
-	"claude-3-7-sonnet-thinking",
-	"claude-3-7-sonnet",
 	"gemini-2.5-pro",
 	"gemini-2.0-flash",
-	"deep-seek-v3",
 	"deep-seek-r1",
-	"claude-sonnet-4-thinking",
 	"claude-sonnet-4",
+	"claude-opus-4",
+	"grok-4-0709",
 
 	"flux",
 	"flux-speed",
@@ -27,26 +23,22 @@ var DefaultOpenaiModelList = []string{
 	"ideogram/V_2A",
 	"recraft-v3",
 	"dall-e-3",
-	"imagen3",
 	"imagen4",
 	"gpt-image-1",
 	"flux-pro/kontext/pro",
+	"flux-pro/kontext/max",
 }
 
 var TextModelList = []string{
 	"gpt-4.1",
-	"o1",
-	"o3",
 	"o3-pro",
 	"o4-mini-high",
-	"claude-3-7-sonnet-thinking",
-	"claude-3-7-sonnet",
 	"gemini-2.5-pro",
 	"gemini-2.0-flash",
-	"deep-seek-v3",
 	"deep-seek-r1",
-	"claude-sonnet-4-thinking",
 	"claude-sonnet-4",
+	"claude-opus-4",
+	"grok-4-0709",
 }
 
 var MixtureModelList = []string{
@@ -63,18 +55,20 @@ var ImageModelList = []string{
 	"ideogram/V_2A",
 	"recraft-v3",
 	"dall-e-3",
-	"imagen3",
 	"imagen4",
 	"gpt-image-1",
 	"flux-pro/kontext/pro",
+	"flux-pro/kontext/max",
 }
 
 var VideoModelList = []string{
-	"kling/v1.6/standard",
-	"pixverse/v3.5/turbo",
-	"lumadream/ray-2",
-	"gemini/veo2",
-	"hunyuan",
+	"gemini/veo3",
+	"gemini/veo3/fast",
+	"kling/v2.1/master",
+	"fal-ai/bytedance/seedance/v1/pro",
+	"minimax/hailuo-02/standard",
+	"pixverse/v4.5/turbo",
+	"fal-ai/bytedance/seedance/v1/lite",
 }
 
 //
